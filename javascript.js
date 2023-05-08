4646aee34b34118c7d09e224aee3027d780d6d42
jags600 <jags50000@gmail.com>
2023-05-08 19:43:31 -0400

    Validaciones

--- javascript.js
+++ javascript.js
@@ -24,53 +24,98 @@ function cargarFooter() {
 window.onload = function() {
 	cargarFooter();
 };
+// objeto.metodo(json)
 
-function validarRut(rut) {
-	// Verificar el formato del RUT
-	if (!/^[0-9]+-[0-9kK]{1}$/.test(rut)) {
-	  return false;
-	}
+$(document).ready(function() {
+
+  // Agregar método de validación para RUT chileno
+  $.validator.addMethod("rutChileno", function(value, element) {
+    // Eliminar puntos y guión del RUT
+    value = value.replace(/[.-]/g, "");
+
+    // Validar que el RUT tenga 8 o 9 dígitos
+    if (value.length < 8 || value.length > 9) {
+      return false;
+    }
+
+    // Validar que el último dígito sea un número o una 'K'
+    var validChars = "0123456789K";
+    var lastChar = value.charAt(value.length - 1).toUpperCase();
+    if (validChars.indexOf(lastChar) == -1) {
+      return false;
+    }
+
+    // Calcular el dígito verificador
+    var rut = parseInt(value.slice(0, -1), 10);
+    var factor = 2;
+    var sum = 0;
+    var digit;
+    while (rut > 0) {
+      digit = rut % 10;
+      sum += digit * factor;
+      rut = Math.floor(rut / 10);
+      factor = factor === 7 ? 2 : factor + 1;
+    }
+    var dv = 11 - (sum % 11);
+    dv = dv === 11 ? "0" : dv === 10 ? "K" : dv.toString();
+
+    // Validar que el dígito verificador sea correcto
+    return dv === lastChar;
+  }, "Por favor ingrese un RUT válido."); 
+
+$("#formulario1").validate({
+  rules: {
+    rut: {
+      required: true,
+      rutChileno: true
+    },
+    apellidos: {
+      required: true      
+    },
+    email: {
+      required: true,
+      email: true,
+      customEmail: /@gmail\.com$/
+    },
+    Direccion: {
+      required: true
+    },
+    password: {
+      required: true,
+      minlength: 5,
+    },
+    password2: {
+      required: true,
+      equalTo: "#password",
+    },
+  }, // --> Fin de reglas
+  messages: {
+    rut: {
+      required: "El rut es un campo obligatorio",
+      rutChileno: "El formato del rut no es válido"
+    },
+    apellidos: {
+      required: "Los apellidos es una campo obligatorio"
+    },
+    email: {
+      required: "Por favor ingrese su correo electrónico",
+      email: "Por favor ingrese un correo electrónico válido"
+    },
+    Direccion: {
+      required: "La direccion es una campo obligatorio"
+    },
+    password: {
+      required: "La contraseña es una campo obligatorio",
+      minlength: "Mínimo 5 caracteres",
+    },
+    password2: {
+      required: "Repita la contraseña anterior",
+      equalTo: "Debe ser igual al campo contraseña",
+    },
+  },
+});
+});
 	
-	// Extraer el dígito verificador y los números del RUT
-	var digits = rut.split("-");
-	var number = digits[0];
-	var verifier = digits[1];
-	
-	// Calcular el dígito verificador esperado
-	var expectedVerifier = calcularDigitoVerificador(number);
-	
-	// Comparar el dígito verificador esperado con el dígito verificador actual
-	return verifier.toLowerCase() === expectedVerifier.toLowerCase();
-  }
-  
-  function calcularDigitoVerificador(rut) {
-	var sum = 0;
-	var multiplier = 2;
-	
-	// Sumar los productos de los dígitos por el multiplicador
-	for (var i = rut.length - 1; i >= 0; i--) {
-	  sum += parseInt(rut.charAt(i)) * multiplier;
-	  
-	  if (multiplier === 7) {
-		multiplier = 2;
-	  } else {
-		multiplier++;
-	  }
-	}
-	
-	// Calcular el dígito verificador a partir de la suma
-	var remainder = sum % 11;
-	var verifier = 11 - remainder;
-	
-	if (verifier === 11) {
-	  return "0";
-	} else if (verifier === 10) {
-	  return "k";
-	} else {
-	  return verifier.toString();
-	}
-  }
-  
 
 // Fetch para el api
 fetch('https://fakestoreapi.com/products')
@@ -93,3 +138,28 @@ fetch('https://fakestoreapi.com/products')
   });
 
 
+  $(document).ready(function() {
+    $("#inicio-sesion").validate({
+      rules: {
+        correo: {
+          required: true,
+          email: true,
+          customEmail: /@gmail\.com$/
+        },
+        contraseña: {
+          required: true,
+          minlength: 6
+        }
+      },
+      messages: {
+        correo: {
+          required: "Por favor ingrese su correo electrónico",
+          email: "Por favor ingrese un correo electrónico válido"
+        },
+        contraseña: {
+          required: "Por favor ingrese su contraseña",
+          minlength: "Su contraseña debe tener al menos 6 caracteres"
+        }
+      }
+    });
+  });
